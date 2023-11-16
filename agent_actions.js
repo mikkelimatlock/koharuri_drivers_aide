7ed50abab96b64a6205b4b611d1f8cd8d72e14e8
Mikkeli Matlock <mikkeli@Matlock-G14>
2023-11-16 18:46:44 +0900

    updated readme, implemented shaking rikka animation

--- agent_actions.js
+++ agent_actions.js
@@ -3,17 +3,26 @@
 document.addEventListener('keydown', function(event) {
     if (event.key === 'a') { // Replace with appropriate trigger
         // changeAgentState('happy.png', 'happy.mp3');
-        changeAgentState('images/angry.png', 'voices/angry.mp3')
+        changeAgentState('angry', 'voices/angry.mp3')
     }
+    // Fix audio path later
     if (event.key === 's') {
-      changeAgentState('images/default.png', 'voices/default.mp3')
+      changeAgentState('default', 'voices/default.mp3')
     }
     // Add more conditions as needed
 });
 
-function changeAgentState(portrait, sound) {
-    document.getElementById('agentPortrait').src = portrait;
-    // let audioElement = document.getElementById('agentAudio');
-    // audioElement.src = sound;
-    // audioElement.play();
+function changeAgentState(state, sound) {
+  let portrait = 'images/' + state + '.png';
+  let agentPortrait = document.getElementById('agentPortrait')
+  agentPortrait.src = portrait;
+  if (state != 'default') {
+    agentPortrait.classList.add('shaking_heavy');
+  }
+  else {
+    agentPortrait.classList.remove('shaking_heavy');
+  }
+  // let audioElement = document.getElementById('agentAudio');
+  // audioElement.src = sound;
+  // audioElement.play();
 }
