600d5d0edade67c67022c808d54b6d7e64ec4043
Mikkeli Matlock <mikkeli@Matlock-G14>
2023-11-16 15:35:38 +0900

    portrait size dynamic limiter & basic emotion switch

--- agent_actions.js
+++ agent_actions.js
@@ -2,14 +2,18 @@
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'a') { // Replace with appropriate trigger
-        changeAgentState('happy.jpg', 'happy.mp3');
+        // changeAgentState('happy.png', 'happy.mp3');
+        changeAgentState('images/angry.png', 'voices/angry.mp3')
+    }
+    if (event.key === 's') {
+      changeAgentState('images/default.png', 'voices/default.mp3')
     }
     // Add more conditions as needed
 });
 
 function changeAgentState(portrait, sound) {
     document.getElementById('agentPortrait').src = portrait;
-    let audioElement = document.getElementById('agentAudio');
-    audioElement.src = sound;
-    audioElement.play();
+    // let audioElement = document.getElementById('agentAudio');
+    // audioElement.src = sound;
+    // audioElement.play();
 }
