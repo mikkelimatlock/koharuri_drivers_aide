f588ae5231c6bd94156000d5058e1c4d1842f717
Mikkeli Matlock <mikkeli@Matlock-ter>
2023-11-17 16:10:40 +0900

    voice changes with states, 2 levels of shaking

--- agent_actions.js
+++ agent_actions.js
@@ -5,6 +5,9 @@ document.addEventListener('keydown', function(event) {
         // changeAgentState('happy.png', 'happy.mp3');
         changeAgentState('angry', 'voices/angry.mp3')
     }
+    if (event.key === 'd') {
+      changeAgentState('composed', 'voices/composed.mp3')
+    }
     // Fix audio path later
     if (event.key === 's') {
       changeAgentState('default', 'voices/default.mp3')
@@ -12,17 +15,51 @@ document.addEventListener('keydown', function(event) {
     // Add more conditions as needed
 });
 
-function changeAgentState(state, sound) {
+function voiceRandomiser(state) {
+  /* List of available voices for each state */
+  const voices = {
+    'angry': ['voices/rikka/angry1.wav']
+  }; 
+  //
+  /* Default state does not have voices */
+  if (state != 'default') {
+    if (voices[state] && voices[state].length > 0) {
+      let randomIndex = Math.floor(Math.random() * voices[state].length);
+      return voices[state][randomIndex];
+    }
+  }
+  else return null;
+}
+
+function changeAgentState(state) {
+  /* a face changer */
   let portrait = 'images/' + state + '.png';
   let agentPortrait = document.getElementById('agentPortrait')
   agentPortrait.src = portrait;
+
+  let agentAudio = document.getElementById('agentAudio');
+  
+
+  /* animations and fixing the div element */ 
+  const shakingIntensity = {
+    'angry': 'shaking_heavy',
+    'composed': 'shaking_light'
+  };
+  
   if (state != 'default') {
-    agentPortrait.classList.add('shaking_heavy');
-  }
-  else {
+    /* clear any existing shakings */
     agentPortrait.classList.remove('shaking_heavy');
+    agentPortrait.classList.remove('shaking_light');
+
+    agentPortrait.classList.add(shakingIntensity[state]);
+    let newVoicePath = voiceRandomiser(state);
+    agentAudio.src = newVoicePath;
+    agentAudio.play();
   }
-  // let audioElement = document.getElementById('agentAudio');
-  // audioElement.src = sound;
-  // audioElement.play();
+  if (state === 'default') {
+    agentPortrait.classList.remove('shaking_heavy');
+    agentPortrait.classList.remove('shaking_light');
+    /* remove the current audio path */
+    agentAudio.src = '';
+  }
 }
